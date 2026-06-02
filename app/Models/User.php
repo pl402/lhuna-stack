31213218d5068fcb44aaa73cd2795f09ee68ae69
Elias Portugal <portugal402@gmail.com>
2026-06-02 13:01:57 -0700

    feat: add cursos relationship to User model and fix indentation in Configuracion model

--- app/Models/User.php
+++ app/Models/User.php
@@ -101,7 +101,10 @@ class User extends Authenticatable
             ->orWhere('id', 'LIKE', "%{$key}%");
     }
 
-                    // -- ENTITY DESIGNER RELATIONS START --
-
+                                    // -- ENTITY DESIGNER RELATIONS START --
+    public function cursos()
+    {
+        return $this->hasMany(\App\Models\Designer\Curso::class, 'user_id');
+    }
     // -- ENTITY DESIGNER RELATIONS END --
 }
